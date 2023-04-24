6407e89c79919da540fb5340504a69cba16c188a
Max Hollmann <maxhollmann@gmail.com>
2023-04-25 01:24:49 +0200

    Enter goes to focused element if any instead of first link

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   let links = process.env.LINKS;
   let search = "";
   let searchElement;
+  let activeElement = null;
 
   $: filteredLinks = filterLinks(links, search);
 
@@ -18,9 +19,16 @@
 
     if (event.key == 'Escape') {
       searchElement.clear();
-    } else if (event.key == 'Enter') {
-      if (filteredLinks.length > 0)
+      if (activeElement) {
+        activeElement.blur();
+      }
+    } else if (event.key == 'Enter'
+               || event.target != searchElement.getInput() && event.key == ' ') {
+      if (activeElement && activeElement.classList.contains('link') && activeElement.href) {
+        window.location = activeElement.href;
+      } else if (filteredLinks.length > 0) {
         window.location = filteredLinks[0].url;
+      }
     } else if (event.target != searchElement.getInput() && event.key.length == 1) {
       searchElement.reset(event.key)
     } else {
@@ -29,6 +37,14 @@
 
     event.preventDefault();
   }
+
+  const updateActiveElement = (event) => {
+    activeElement = document.activeElement;
+  }
+
+  document.addEventListener('focus', updateActiveElement, true)
+  document.addEventListener('blur', updateActiveElement, true)
+
 </script>
 
 
@@ -36,7 +52,7 @@
 </style>
 
 
-<svelte:window on:keydown={handleKeydown}/>
+<svelte:window on:keydown={handleKeydown} />
 
 <svelte:head>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.2.96/css/materialdesignicons.min.css">
